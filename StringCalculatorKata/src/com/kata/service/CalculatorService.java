c2b827cb55ccfccb1a5588753b0051e9f274baa4
michalevn <kimlongap@gmail.com>
2015-09-21 10:25:36 +0700

    init project on git

---
+++ StringCalculatorKata/src/com/kata/service/CalculatorService.java
@@ -0,0 +1,5 @@
+package com.kata.service;
+
+public interface CalculatorService {
+
+}
---
+++ StringCalculatorKata/src/com/kata/test/StringCalculatorTest.java
@@ -0,0 +1,14 @@
+package com.kata.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StringCalculatorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
